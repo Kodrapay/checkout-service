36e7c6f3c9b2dffbc31770015fbbd31f35b1fe64
KodraPay Dev <dev@kodrapay.com>
2025-12-04 01:55:26 +0100

    Containerize

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,20 +2,10 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/kodra-pay/checkout-service/internal/config"
 	"github.com/kodra-pay/checkout-service/internal/handlers"
-	"github.com/kodra-pay/checkout-service/internal/repositories"
-	"github.com/kodra-pay/checkout-service/internal/services"
 )
 
-func Register(app *fiber.App, cfg config.Config, repo *repositories.CheckoutRepository) {
-	health := handlers.NewHealthHandler(cfg.ServiceName)
+func Register(app *fiber.App, serviceName string) {
+	health := handlers.NewHealthHandler(serviceName)
 	health.Register(app)
-
-	svc := services.NewCheckoutService(repo)
-	h := handlers.NewCheckoutHandler(svc)
-	api := app.Group("/checkout")
-	api.Post("/sessions", h.CreateSession)
-	api.Get("/sessions/:id", h.GetSession)
-	api.Post("/pay", h.Pay)
 }
